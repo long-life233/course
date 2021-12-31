f0eac853bc52b2ff98e80d7001504d148ffd998a
LuKecheng <2443876261@qq.com>
2021-12-31 17:57:01 +0800

    侧边栏分组失败

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,33 +6,61 @@ module.exports = {
             { text: 'vue3', link: '/vue3/1.创建vue3项目.md' },
             { text: 'blender', link: '/blender/Blender教程.md' }
         ],
-        sidebar: {
-            '/vue3/':[
+        sidebar: [
+            {
+              title: 'Group 1',   // 必要的
+              path: '/vue3/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+              collapsable: false, // 可选的, 默认值是 true,
+              sidebarDepth: 1,    // 可选的, 默认值是 1
+              children: [
                 '1.创建vue3项目.md',
-                '2.defineComponent函数.md',
-                '3.setup.md',
-                '4.ref.md',
-                '5.reactive.md',
-                '6.比较vue2和vue3的响应式.md',
-                '7.setup细节.md',
-                '8.计算属性与监视函数.md',
-                '9.生命周期.md',
-                '10.自定义hook.md',
-                '11.toRefs.md',
-                '12.shallowReactive和shallowRef.md',
-                '13.readonly 与 shallowReadonly.md',
-                '14.新组件.md',
-                '15.toRaw,markRaw,toRef,customRef,provide_inject.md',
-                '16.响应式数据的判断.md',
-                '17.todoList练习.md',
-            ],
-            '/blender/':[
-                'Blender教程.md'
-            ]
-        }
+                        '2.defineComponent函数.md',
+                        '3.setup.md',
+                        '4.ref.md',
+                        '5.reactive.md',
+                        '6.比较vue2和vue3的响应式.md',
+                        '7.setup细节.md',
+                        '8.计算属性与监视函数.md',
+                        '9.生命周期.md',
+                        '10.自定义hook.md',
+                        '11.toRefs.md',
+                        '12.shallowReactive和shallowRef.md',
+                        '13.readonly 与 shallowReadonly.md',
+                        '14.新组件.md',
+                        '15.toRaw,markRaw,toRef,customRef,provide_inject.md',
+                        '16.响应式数据的判断.md',
+                        '17.todoList练习.md',
+              ]
+            }
+          ]
     },
     description: 'thunderpowers-course',// 网站的描述，它将会以 <meta> 标签渲染到当前页面的 HTML 中
     head: [
         ['link', { rel: 'icon', href: '/logo.png' }],// 浏览器标签的icon，/表示在public目录下去找
     ]
 }
+
+// {
+//     '/vue3/':[
+//         '1.创建vue3项目.md',
+//         '2.defineComponent函数.md',
+//         '3.setup.md',
+//         '4.ref.md',
+//         '5.reactive.md',
+//         '6.比较vue2和vue3的响应式.md',
+//         '7.setup细节.md',
+//         '8.计算属性与监视函数.md',
+//         '9.生命周期.md',
+//         '10.自定义hook.md',
+//         '11.toRefs.md',
+//         '12.shallowReactive和shallowRef.md',
+//         '13.readonly 与 shallowReadonly.md',
+//         '14.新组件.md',
+//         '15.toRaw,markRaw,toRef,customRef,provide_inject.md',
+//         '16.响应式数据的判断.md',
+//         '17.todoList练习.md',
+//     ],
+//     '/blender/':[
+//         'Blender教程.md'
+//     ]
+// }
