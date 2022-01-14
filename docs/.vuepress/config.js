e0f61a928c57672a217674febd7b6afa5536fbd5
lukecheng <2443876261@qq.com>
2022-01-14 10:11:40 +0800

    假如她比我强

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -56,7 +56,8 @@ module.exports = {
                         'article/2021.1.11-the-little-red-writing-book-exercise',
                         'article/2022.1.12-big-error-in-sourceTree',
                         'article/2022.1.12-my-and-other',
-                        'article/2022.1.13-good-html'
+                        'article/2022.1.13-good-html',
+                        'article/2022.1.14-if-she-is-better-than-me'
                     ]
                 }
                
