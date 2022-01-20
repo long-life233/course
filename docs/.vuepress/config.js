4479faefe44f505da06b0dd211b2a609f0f1d737
lukecheng <2443876261@qq.com>
2022-01-20 21:40:48 +0800

    变更结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,69 +3,77 @@ module.exports = {
     themeConfig: {
         logo: '/logo.png',
         nav: [
-            { text: 'vue3', link: '/vue3/' },
-            { text: 'talk', link: '/thinking/' }
+            { text: 'html-css-js', link: '/html-css-js/' },
+            { text: 'vue', link: '/vue/' },
+            { text: 'uniapp', link: '/uniapp/' },
+            { text: 'sundry', link: '/sundry/' },
+            { text: 'other', link: '/other/' }
         ],
         sidebarDepth: 3,
         lastUpdated: '最后更新于', // string | boolean
-        
+
         sidebar: {
-            '/vue3/': [
+            '/html-css-js/': [
+                '',
+                'article/2022.1.13-good-html',
+                'article/2022.1.16-0-base-front-end',
+                'article/2022.1.17-css-layout',
+                'article/2022.1.17-js-advanced'
+            ],
+            '/vue/': [
                 {
                     title: "基础部分",
                     collapsable: false,
                     children: [
                         '',
-                        'base/compare-vue2-and-vue3',
-                        'base/use-v-model-in-diy-component',
-                        'base/computed-watch',
-                        'base/create-vue3-project',
-                        'base/defineComponent',
-                        'base/hook',
-                        'base/life-cycle',
-                        'base/new-compoent',
-                        'base/reactive',
-                        'base/readonly-shallowReadonly',
-                        'base/ref',
-                        'base/responsvie',
-                        'base/setup-detail',
-                        'base/setup',
-                        'base/shallowReactive-shallowRef',
-                        'base/toRaw-markRaw-toRef-customRef-provide-inject',
-                        'base/toRefs'
+                        'article/compare-vue2-and-vue3',
+                        'article/use-v-model-in-diy-component',
+                        'article/computed-watch',
+                        'article/create-vue3-project',
+                        'article/defineComponent',
+                        'article/hook',
+                        'article/life-cycle',
+                        'article/new-compoent',
+                        'article/reactive',
+                        'article/readonly-shallowReadonly',
+                        'article/ref',
+                        'article/responsvie',
+                        'article/setup-detail',
+                        'article/setup',
+                        'article/shallowReactive-shallowRef',
+                        'article/toRaw-markRaw-toRef-customRef-provide-inject',
+                        'article/toRefs',
+                        'article/2022.1.11-vue3-todoList-summary'
 
                     ]
                 },
                 {
-                    title: "练习",
+                    title: "练习部分",
                     collapsable: false,
                     children: [
-                        'project/todoList-exercise'
+                        'article/todoList-exercise'
                     ]
                 }
             ],
-            '/thinking/':[
-                {
-                    title:"练习写作",
-                    collapsable:false,
-                    children:[
-                        '',
-                        'article/2022.1.11-write',
-                        'article/2022.1.11-vue3-todoList-summary',
-                        'article/2022.1.11-community-project-summary',
-                        'article/2021.1.11-the-little-red-writing-book-exercise',
-                        'article/2022.1.12-big-error-in-sourceTree',
-                        'article/2022.1.12-my-and-other',
-                        'article/2022.1.13-good-html',
-                        'article/2022.1.14-if-she-is-better-than-me',
-                        'article/2022.1.16-0-base-front-end',
-                        'article/2022.1.17-css-layout',
-                        'article/2022.1.17-js-advanced',
-                        'article/2022.1.20-love-you-feel-sad',
-                        'article/2022.1.20-talk-show-wow'
-                    ]
-                }
-               
+            '/uniapp/': [
+                '',
+                'article/2022.1.11-community-project-summary'
+            ],
+            '/sundry/': [
+                '',
+                'article/2022.1.12-big-error-in-sourceTree'
+            ],
+            '/other/': [
+                '',
+                'article/2022.1.7-self-closing',
+                'article/2022.1.11-write',
+                'article/2022.1.11-the-little-red-writing-book-exercise',
+                'article/2022.1.12-my-and-other',
+                'article/2022.1.14-if-she-is-better-than-me',
+                'article/2022.1.15-but-she-not-love-you',
+                'article/2022.1.20-how-speak-fluently',
+                'article/2022.1.20-love-you-feel-sad',
+                'article/2022.1.20-talk-show-wow'
             ]
         }
     },
--- docs/README.md
+++ docs/README.md
@@ -2,12 +2,7 @@
 home: true
 heroImage: /logo.png
 actionText: 快速上手 →
-actionLink: /vue3/
+actionLink: /vue/
 features:
 ---
-<!-- 
 
-介绍侧边栏博客 
-https://blog.csdn.net/wq_ocean_/article/details/109220650 
-
--->
