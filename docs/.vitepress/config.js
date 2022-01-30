6cfee58c0f2bd4298be20e5ee926048cfffc5a3f
lukecheng <2443876261@qq.com>
2022-01-30 20:45:49 +0800

    冒泡排序，自卑原因

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -15,6 +15,7 @@ module.exports = {
             { text: 'java', link: '/java/' },
             { text: 'vue', link: '/vue/' },
             { text: 'uniapp', link: '/uniapp/' },
+            {text:'interview',link:'/interview/'},
             { text: 'sundry', link: '/sundry/' },
             { text: 'other', link: '/other/' }
         ],
@@ -23,76 +24,95 @@ module.exports = {
         sidebar: {
             '/html-css-js/': [
                 {
-                    text: "介绍",
-                    link: '/html-css-js/',
-                    collapsable: true
+                    text:"html-css-js",
+                    collapsable:false,
+                    children:[
+                        {
+                            text: "介绍",
+                            link: '/html-css-js/',
+                            collapsable: true
+                        },
+                        {
+                            text: "被低估的html",
+                            link: '/html-css-js/article/2022.1.13-good-html',
+                            collapsable: true
+                        },
+                        {
+                            text: "零基础入门前端",
+                            link: '/html-css-js/article/2022.1.16-0-base-front-end',
+                            collapsable: true
+                        },
+                        {
+                            text: "CSS布局",
+                            link: '/html-css-js/article/2022.1.17-css-layout',
+                            collapsable: true
+                        },
+                        {
+                            text:"样式相关",
+                            link:'/html-css-js/article/2022.1.30-about-ui',
+                            collapsable:true
+                        }
+                        // {
+                        //     text:"JS进阶",
+                        //     link:'/html-css-js/article/2022.1.17-js-advanced',
+                        //     collapsable: true
+                        // },
+                    ]
                 },
-                {
-                    text: "被低估的html",
-                    link: '/html-css-js/article/2022.1.13-good-html',
-                    collapsable: true
-                },
-                {
-                    text: "零基础入门前端",
-                    link: '/html-css-js/article/2022.1.16-0-base-front-end',
-                    collapsable: true
-                },
-                {
-                    text: "CSS布局",
-                    link: '/html-css-js/article/2022.1.17-css-layout',
-                    collapsable: true
-                },
-                // {
-                //     text:"JS进阶",
-                //     link:'/html-css-js/article/2022.1.17-js-advanced',
-                //     collapsable: true
-                // },
+                
             ],
             '/java/': [
                 {
-                    text: "介绍",
-                    link: "/java/"
-                },
-                {
-                    text: "第一天",
-                    link: "/java/article/1-day"
-                },
-                {
-                    text: "第二天",
-                    link: "/java/article/2-day"
-                },
-                {
-                    text: "面向对象编程（上）",
-                    link: "/java/article/3-day"
-                },
-                {
-                    text: "面向对象编程（中）",
-                    link: "/java/article/4-day"
-                },
-                {
-                    text: "面向对象编程（下）",
-                    link: "/java/article/5-day"
+                    text:"复习",
+                    children:[
+                        {
+                            text: "介绍",
+                            link: "/java/"
+                        },
+                        {
+                            text: "第一天",
+                            link: "/java/article/1-day"
+                        },
+                        {
+                            text: "第二天",
+                            link: "/java/article/2-day"
+                        },
+                        {
+                            text: "面向对象编程（上）",
+                            link: "/java/article/3-day"
+                        },
+                        {
+                            text: "面向对象编程（中）",
+                            link: "/java/article/4-day"
+                        },
+                        {
+                            text: "面向对象编程（下）",
+                            link: "/java/article/5-day"
+                        }
+                    ]
                 }
+            ],
+            '/interview/':[
+                {text:"前端",collapsable:true,children:[
+                    {text:"介绍",link:'/interview/'},
+                    {text:"前端面试常见问题",link:'/interview/faq'}
+                ]},
+                
             ],
             '/other/': [
                 {
                     text:"2021",
-                    collapsable:false,
+                    collapsable:true,
                     children:[
                         {
-                            text: "2021",
-                            children: [
-                                {
-                                    text: "dd",
-                                    link: "/other/2021/vedio_gongzhonghao_xiaohua_comment"
-                                }
-                            ]
+                            text: "dd",
+                            link: "/other/2021/vedio_gongzhonghao_xiaohua_comment"
                         }
                     ]
                 },
                 {
                     text: "2022",
-                    collapsable: false,
+                    collapsable: true,
                     children: [
                         {
                             text: "介绍",
@@ -150,6 +170,10 @@ module.exports = {
                         {
                             text: "又是伤心的一天",
                             link: '/other/article/2022.1.30-a-sad-day-again'
+                        },
+                        {
+                            text:"如何不自卑",
+                            link:'/other/article/2022.1.30-how-not-self-pity'
                         }
                     ]
                 },
@@ -166,33 +190,42 @@ module.exports = {
                 }
             ],
             '/uniapp/': [
-                { text: '介绍', link: '/uniapp/' },
-                {
-                    text: 'uniapp仿糗事百科项目总结',
-                    link: '/uniapp/article/2022.1.11-community-project-summary'
-                },
-                {
-                    text: "uni-app结合云函数开发小程序博客",
-                    link: '/uniapp/article/2022.1.29-UNI-App combined with cloud function development applet blog'
-                }
+                {text:'uniapp',children:[
+                    { text: '介绍', link: '/uniapp/' },
+                    {
+                        text: 'uniapp仿糗事百科项目总结',
+                        link: '/uniapp/article/2022.1.11-community-project-summary'
+                    },
+                    {
+                        text: "uni-app结合云函数开发小程序博客",
+                        link: '/uniapp/article/2022.1.29-UNI-App combined with cloud function development applet blog'
+                    }
+                ]},
+                
             ],
             '/sundry/': [
                 {
-                    text: '介绍',
-                    link: '/sundry/'
+                    text:"杂项",
+                    children:[
+                        {
+                            text: '介绍',
+                            link: '/sundry/'
+                        },
+                        {
+                            text: '丢弃，sourceTree的重大失误',
+                            link: "/sundry/article/2022.1.12-big-error-in-sourceTree"
+                        },
+                        {
+                            text: "读计算机书籍的意义",
+                            link: "/sundry/article/2022.1.24-read-book-meaning"
+                        },
+                        {
+                            text: "手写常见代码",
+                            link: "/sundry/article/2022.1.29-hand-common-code"
+                        }
+                    ]
                 },
-                {
-                    text: '丢弃，sourceTree的重大失误',
-                    link: "/sundry/article/2022.1.12-big-error-in-sourceTree"
-                },
-                {
-                    text: "读计算机书籍的意义",
-                    link: "/sundry/article/2022.1.24-read-book-meaning"
-                },
-                {
-                    text: "手写常见代码",
-                    link: "/sundry/article/2022.1.29-hand-common-code"
-                }
+                
             ]
         }
     }
