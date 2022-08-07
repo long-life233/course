ccbbfcd6d8c5870781c85db86ea6701ebf2dc43b
lukecheng <lukecheng233@163.com>
2022-08-07 15:29:28 +0800

    feat: 以?raw后缀结尾直接将文件内容以string形式引入

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,77 +1,11 @@
 <script setup>
 import { onMounted, reactive, ref } from 'vue'
 import { marked } from 'marked'
-import TextareaEditor from '@/components/TextareaEditor.vue'
-import request from '@/utils/request.js'
-import vueMd from '@/assets/vue.md?raw'
-import reactMd from '@/assets/react.md?raw'
-
-
-//#region vue
-const vueData = ref('')
-const vueEaditor = ref(null)
-
-
-async function editVue() {
-  vueEaditor.value.show()
-}
-
-async function getVue() {
-  // 发请求
-  const res = await request.get('/vue')
-  vueData.value = res.data
-}
-
-
-async function saveVue(content) {
-  if(!content) {
-    return 
-  }
-  // 发请求
-  // await request.post('/vue', {
-  //   content
-  // })
-  getVue()
-
-  localStorage.setItem('vue', content)
-}
-
-//#endregion
-
-//#region react
-const reactData = ref('')
-const reactEaditor = ref(null)
-
-async function editReact() {
-  reactEaditor.value.show()
-}
-
-
-async function getReact() {
-  // 发请求
-  const res = await request.get('/react')
-  reactData.value = res.data
-}
-
-async function saveReact(content) {
-  if(!content) {
-    return
-  }
-  // 发请求
-  // await request.post('/react', {
-  //   content
-  // })
-  getReact()
-
-  localStorage.setItem('react', content)
-}
-//#endregion
-
+import vueMd from '@/docs/vue.md?raw'
+import reactMd from '@/docs/react.md?raw'
 
 onMounted(async () => {
-  // 发请求
-  getVue()
-  getReact()
+  
 })
 </script>
 
@@ -81,16 +15,15 @@ onMounted(async () => {
       <div class="px-4 my-3 flex items-center">
         <div class="w-1 h-10 bg-green-600 rounded-lg"></div>
         <span class="text-5xl font-black mx-5">Vue</span>
-        <div @click="editVue" class="px-3 py-1 rounded-lg bg-stone-900 text-white cursor-pointer">编辑</div>
       </div>
       <article class="prose prose-sm max-w-full p-3 bg-slate-200">
-        <div v-html="marked(vueData)"></div>
+        <div v-html="marked(vueMd)"></div>
       </article>
     </div>
     <!--  -->
     <div class="h-full flex-none flex justify-center" style="width:10rem">
       <div class="w-3/4 flex justify-center bg-sky-500 self-start my-6 p-2 rounded-md" @click="addBlock">
-        添加一个标题
+        ——————
       </div>
     </div>
     <!--  -->
@@ -98,16 +31,11 @@ onMounted(async () => {
       <div class="px-4 my-3 flex items-center">
         <div class="w-1 h-10 bg-green-600 rounded-lg"></div>
         <span class="text-5xl font-black mx-5">React</span>
-        <div @click="editReact" class="px-3 py-1 rounded-lg bg-stone-900 text-white cursor-pointer">编辑</div>
       </div>
       <article class="prose prose-sm max-w-full p-3 bg-slate-200">
-        <div v-html="marked(reactData)"></div>
+        <div v-html="marked(reactMd)"></div>
       </article>
       
     </div>
   </div>
-
-  <!-- 编辑弹框 -->
-  <textarea-editor ref="vueEaditor" :content="vueData" @hide="saveVue"></textarea-editor>
-  <textarea-editor ref="reactEaditor" :content="reactData" @hide="saveReact"></textarea-editor>
 </template>
